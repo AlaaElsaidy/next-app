510494b50959a66894d90f0f0c28b8f3cfde0a5d
Alaa <AlaaElsaidy2003@gmail.com>
2025-10-17 23:10:11 +0300

    first commit

--- app/(main)/contact/page.js
+++ app/(main)/contact/page.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 export default function page() {
-    throw new Error('Test Error ')
   return (
-    <div>
-      
+  
+      <div className="min-h-screen flex flex-col items-center justify-center bg-[#0a192f] text-orange-400 font-sans px-6 sm:px-12 py-16">
+        <h1 className="text-5xl sm:text-6xl font-extrabold drop-shadow-lg">
+          About Us
+        </h1>
+    
     </div>
+  
   )
 }
